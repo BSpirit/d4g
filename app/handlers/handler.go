cc41cc7441c03e62e499ca25ea484de80c76dd86
grooms <sauvaire.romain@gmail.com>
2019-11-21 17:29:54 +0100

    cors handled

--- app/handlers/handler.go
+++ app/handlers/handler.go
@@ -32,9 +32,16 @@ type Handler struct {
 
 // ServeHTTP allows our Handler type to satisfy http.Handler.
 func (h Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	enableCors(&w)
 	err := h.HandlerFunc(h.Env, w, r)
 	if err != nil {
 		log.Printf("%s", err.Unwrap())
 		http.Error(w, http.StatusText(err.Code), err.Code)
 	}
+}
+
+func enableCors(w *http.ResponseWriter) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 }
